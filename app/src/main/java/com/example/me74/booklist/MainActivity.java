4e129f37be5d7704cceb3d690c9c55eae9d9ff8c
sun-strider <mevola8@gmail.com>
2017-07-14 21:43:33 +0200

    Search Button option of app

--- app/src/main/java/com/example/me74/booklist/MainActivity.java
+++ app/src/main/java/com/example/me74/booklist/MainActivity.java
@@ -9,10 +9,9 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -55,8 +54,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Log.e(LOG_TAG, "onCreate \n" + fullQueryUrl);
         setContentView(R.layout.activity_main);
 
         // Hide the loading indicator first
@@ -69,7 +66,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // Find a reference to the {@link ListView} in the layout
         ListView bookListView = (ListView) findViewById(R.id.list);
 
-        // Set the empty state view
+        // Find the search TextView
+        TextView search = (TextView) findViewById(R.id.search_button);
+
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
         bookListView.setEmptyView(mEmptyStateTextView);
 
@@ -77,39 +76,19 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // so the list can be populated in the user interface
         bookListView.setAdapter(mAdapter);
 
-
-        // Find a reference to the {@link SearchView} in the layout
-        SearchView searchView = (SearchView) findViewById(R.id.search_view);
-
-        // Query hint on the SearchView box
-        searchView.setQueryHint(getString(R.string.search_books));
-
-        // Set the SearchView box active
-        searchView.setIconifiedByDefault(false);
-
-        //Set an OnQueryTextListener to the search button
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
+        // Set a click listener on that View
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextChange(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextSubmit(String newText) {
+            public void onClick(View v) {
+                mAdapter.clear();
                 // Show the loading indicator
                 View loadingIndicator = findViewById(R.id.loading_indicator);
                 loadingIndicator.setVisibility(View.VISIBLE);
-
-                SearchView searchView = (SearchView) findViewById(R.id.search_view);
-                String searchString = searchView.getQuery().toString();
-
                 // Get the input and make an search Url out of it
-                fullQueryUrl = makeQueryUrl(searchString);
-
+                fullQueryUrl = makeQueryUrl();
                 // Start the search (the data query)
-                getLoaderManager().restartLoader(BOOK_LOADER_ID, null, MainActivity.this);
-                return true;
+                startSearch();
+
             }
         });
 
@@ -133,7 +112,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
+        LoaderManager loaderManager = getLoaderManager();
 
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(BOOK_LOADER_ID, null, this);
+    }
+
+
+    // This method will be called when the search is started
+    private void startSearch() {
 
         // Get a reference to the ConnectivityManager to check state of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager)
@@ -151,10 +140,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
             // because this activity implements the LoaderCallbacks interface).
             loaderManager.initLoader(BOOK_LOADER_ID, null, this);
+            if (loaderManager.getLoader(BOOK_LOADER_ID).isStarted()) {
+                //restart it if there's one
+                getLoaderManager().restartLoader(BOOK_LOADER_ID, null, this);
+            }
         } else {
             // Otherwise, display error
             // First, hide loading indicator so error message will be visible
-            loadingIndicator = findViewById(R.id.loading_indicator);
+            View loadingIndicator = findViewById(R.id.loading_indicator);
             loadingIndicator.setVisibility(View.GONE);
 
             // Update empty state with no connection error message
@@ -162,8 +155,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     }
 
-    private String makeQueryUrl(String searchString) {
+    private String makeQueryUrl() {
         String url = GOOGLE_BOOKS_REQUEST_URL;
+        EditText searchView = (EditText) findViewById(R.id.search_view);
+        String searchString = searchView.getText().toString();
         searchString = searchString.replace(" ", "+");
         url = url + searchString;
         return url;
@@ -174,8 +169,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<Book>> onCreateLoader(int i, Bundle bundle) {
-        Log.e(LOG_TAG, "onCreateLoader \n" + fullQueryUrl);
-
         // Create a new loader for the given URL
         return new BookLoader(this, fullQueryUrl);
     }
@@ -206,4 +199,3 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mAdapter.clear();
     }
 }
-
